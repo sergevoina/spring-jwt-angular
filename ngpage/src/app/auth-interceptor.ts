1a42b11384e65893104592d376a1501cd116ba7b
Serge Voina <serge.voina@gmail.com>
2019-01-22 22:39:58 +1100

    Implement auto refresh access token

--- ngpage/src/app/auth-interceptor.ts
+++ ngpage/src/app/auth-interceptor.ts
@@ -1,46 +1,75 @@
-import { Injectable } from '@angular/core';  
-import {  
-    HttpEvent,  
-    HttpInterceptor,  
-    HttpHandler,  
-    HttpRequest,  
-    HttpHeaders  
-} from '@angular/common/http';  
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';	
+import {	
+	HttpEvent,	
+	HttpInterceptor,	
+	HttpHandler,	
+	HttpRequest,	
+	HttpHeaders,
+	HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
+import { catchError, switchMap, take, filter, finalize } from 'rxjs/operators';
 
-import { AuthService } from './auth.service';
-@Injectable()  
-export class AuthInterceptor implements HttpInterceptor {  
-  
-  	constructor(private authService: AuthService) {}
+import { AuthService, User } from './auth.service';
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+@Injectable()	
+export class AuthInterceptor implements HttpInterceptor {	
+	isRefreshingToken: boolean = false;
+	tokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>(null);
+	
+	constructor(private authService: AuthService) {}
 
-      // checks if the user is logged in by getting the access token from the authService.
- 	  let token = this.authService.getToken();
- 	  if(token) {
- 	    // add the access token to the Authorization header
-  	    req = req.clone({
- 	      setHeaders: {
- 	        Authorization: `Bearer ${token}`
- 	      }
- 	    });
- 	  }
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		if(req.url.endsWith("/oauth/token")) {
+			return next.handle(req);
+		}
+		
+		return next.handle(this.addAuthorizationHeader(req, this.authService.getToken()))
+			.pipe(catchError(err => {
+				if (err.status === 401) {
+					return this.handle401Error(req, next);
+				} 
+				
+				// all other errors are re-thrown up to the calling service 
+				// so an alert error message can be displayed to the user.
+				return throwError(err);
+			}));
+	}
+	
+	private addAuthorizationHeader(req: HttpRequest<any>, token: string) : HttpRequest<any> {
+		if(token) {
+			return req.clone({ setHeaders: { Authorization: `Bearer ${token}`}});
+		}
+		return req;
+	}
+	
+ 	private handle401Error(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		if(!this.isRefreshingToken) {
+			this.isRefreshingToken = true;
+			
+			// Reset here so that the following requests wait until the token
+			// comes back from the refreshToken call.
+			this.tokenSubject.next(null);
+			
+ 			this.authService.refreshToken()
+				.subscribe( 
+					user => {
+						this.isRefreshingToken = false;
+						this.tokenSubject.next(user.accessToken);
+					},
+					error => {
+						this.authService.logout();
+						location.reload(true);
+					});
+		}
 
-      // Intercepts http responses from the api to check if there were any errors. 
-      return next.handle(req).pipe(catchError(err => {
-        // Check for a 401 Unauthorized response 
-        if ([401].indexOf(err.status) !== -1) {
-          // Auto logout if 401 Unauthorized response returned from api
-          this.authService.logout();
-          location.reload(true);
-        }
-        
-        // all other errors are re-thrown up to the calling service 
-        // so an alert error message can be displayed to the user.
-        const error = err.error.message || err.statusText;
-        return throwError(error);
-      }));
-    }
-}  
+		return this.tokenSubject
+			.pipe(
+				filter(token => token != null),
+				take(1),
+				switchMap(token => {
+					return next.handle(this.addAuthorizationHeader(request, token));
+				})
+			);
+ 	}
+}
--- ngpage/src/app/auth.service.ts
+++ ngpage/src/app/auth.service.ts
@@ -6,13 +6,26 @@ import * as jwt_decode from "jwt-decode";
 
 
 export class User {
-    username: string;
-    password: string;
-    authorities: string[];
-    accessToken: string;
-    expires: number;
-    refreshToken: string; 
+	username: string;
+	password: string;
+	authorities: string[];
+	accessToken: string;
+	expires: number;
+	refreshToken: string; 
 }
+
+// {
+//   "access_token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDgxNDIwNDksInVzZXJfbmFtZSI6ImFkbWluIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9BRE1JTiJdLCJqdGkiOiIwZGI3YWNmMi0yN2MwLTQ5ZWUtYjJmMy02NWRjM2M0NDI5YmEiLCJjbGllbnRfaWQiOiJzcHJpbmdqd3QtY2xpZW50Iiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl19.9Q9_s3iHPpz_kbo8WOfSfvWHqCG0ExdVmdxU66laTkY",
+//   "token_type":"bearer",
+//   "refresh_token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbiIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJhdGkiOiIwZGI3YWNmMi0yN2MwLTQ5ZWUtYjJmMy02NWRjM2M0NDI5YmEiLCJleHAiOjE1NDgxNjM1ODIsImF1dGhvcml0aWVzIjpbIlJPTEVfQURNSU4iXSwianRpIjoiODE3Njk4ZTgtMmQ3YS00Y2IzLTkwMGYtNjU3ODU5NzgzOTAwIiwiY2xpZW50X2lkIjoic3ByaW5nand0LWNsaWVudCJ9.Y9GgJUY5ikqEMd7ch8Cm2aGhuVAf4-pWInG5lnTUzW0",
+//   "expires_in":19,
+//   "scope":"read write",
+//   "jti":"0db7acf2-27c0-49ee-b2f3-65dc3c4429ba"
+// }
+//
+// jwt_decode(access_token)
+// { authorities: ["ROLE_USER"], client_id: "springjwt-client", exp: 1547917414, 
+// jti: "51352f16-ed4a-4fdf-b53c-49ca4d4a1b67",  scope: ["read", "write", "trust"], user_name: "user", }
 
 export class OauthResp {
 	access_token: string;
@@ -21,99 +34,112 @@ export class OauthResp {
 	expires_in: number;
 	scope: string;
 	jti: string;
-	
-	// jwt_decode(access_token)
-    // { authorities: ["ROLE_USER"], client_id: "springjwt-client", exp: 1547917414, 
-    // jti: "51352f16-ed4a-4fdf-b53c-49ca4d4a1b67",  scope: ["read", "write", "trust"], user_name: "user", }
 }
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthService {
-  private currentUserSubject: BehaviorSubject<User>;
-  public currentUser: Observable<User>;
+	private currentUserSubject: BehaviorSubject<User>;
+	public currentUser: Observable<User>;
 
-  constructor(private httpClient: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(sessionStorage.getItem('currentUser')));
-    this.currentUser = this.currentUserSubject.asObservable();
-  }
+	constructor(private httpClient: HttpClient) {
+		this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(sessionStorage.getItem('currentUser')));
+		this.currentUser = this.currentUserSubject.asObservable();
+	}
 
-  public get currentUserValue(): User {
-    return this.currentUserSubject.value;
-  }
+	public get currentUserValue(): User {
+		return this.currentUserSubject.value;
+	}
 
-  login(username, password): Observable<boolean> {
-    const headers = {
-      'Authorization': 'Basic ' + btoa('springjwt-client:springjwt-secret'),
-      'Content-type': 'application/x-www-form-urlencoded'
-    }
-    
-    const body = new HttpParams()
-      .set('username', username)
-      .set('password', password)
-      .set('grant_type', 'password');
-      
-    let user: User = { username:username, password:null, authorities: [], accessToken: "", expires: 0, refreshToken: ""};
-    
-    return this.httpClient.post<OauthResp>('/oauth/token', body.toString(), {headers}).pipe(map(data => {
-      
-      try{
-        const token = jwt_decode(data.access_token);
-        
-        // { authorities: ["ROLE_USER"], client_id: "springjwt-client", exp: 1547917414, 
-        // jti: "51352f16-ed4a-4fdf-b53c-49ca4d4a1b67",  scope: ["read", "write", "trust"], user_name: "user", }
-        // console.log(token);
-        
-        user.accessToken = data.access_token;
-        user.refreshToken = data.refresh_token;
-        user.authorities = token.authorities;
-        user.expires = token.exp;
-        
-    		sessionStorage.setItem('currentUser', JSON.stringify(user));
-    		this.currentUserSubject.next(user);
-    		
-    		return true;
-      }
-      catch(error){
-         sessionStorage.removeItem('currentUser');
-         this.currentUserSubject.next(null);
-      
-        return false;
-      }
-    		
-    		
-    }));
-  }
-  
-  logout(): void {
-    sessionStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null);
-  }
-  
-  getToken(): string {
-    try {
-      let user = sessionStorage.getItem('currentUser');
-      if(user) {
-        return JSON.parse(user).accessToken;
-      }
-    } catch(error) {
-    	
-    }
- 	return null;
-  }
-  
-  authorise(allowedRoles: string[]): boolean {
-    
-    if(this.currentUserSubject.value && this.currentUserSubject.value.authorities) {
-    	  if(! allowedRoles) {
-    	  	return true;
-    	  } 
+	// Observable<ICurrentUser>
+	login(username, password): Observable<User> {
+		const body = new HttpParams()
+			.set('username', username)
+			.set('password', password)
+			.set('grant_type', 'password');
 		
-      // intersection - at least one role match    	  
-    	  return allowedRoles.filter(value => -1 !== this.currentUserSubject.value.authorities.indexOf(value)).length > 0;
-    }
+		return this.oauth(body.toString());
+	}
+	
+	refreshToken(): Observable<User> {
+		const user = JSON.parse(sessionStorage.getItem('currentUser'));
+	
+		const body = new HttpParams()
+			.set('username', user.username)
+			.set('refresh_token', user.refreshToken)
+			.set('grant_type', 'refresh_token');
 
-  	return false;
-  }
+		return this.oauth(body.toString());
+	}
+	
+	logout(): void {
+		sessionStorage.removeItem('currentUser');
+		this.currentUserSubject.next(null);
+	}
+	
+	private oauth(body: string): Observable<User> {
+		const headers = {
+			'Authorization': 'Basic ' + btoa('springjwt-client:springjwt-secret'),
+			'Content-type': 'application/x-www-form-urlencoded'
+		}
+
+		return this.httpClient
+			.post<OauthResp>('/oauth/token', body, {headers})
+			.pipe(
+				map(oauthResp => {
+					try{
+						let user = this.createUser(oauthResp);
+						
+						sessionStorage.setItem('currentUser', JSON.stringify(user));
+						this.currentUserSubject.next(user);
+						
+						return user;
+					} catch(error) {
+						sessionStorage.removeItem('currentUser');
+						this.currentUserSubject.next(null);
+						
+						return null;
+					}
+			}));
+	}
+	
+	private createUser(oauthResp: OauthResp ): User {
+		const accessToken = jwt_decode(oauthResp.access_token);
+	
+		return { 
+			username: accessToken.user_name, 
+			password:null, 
+			authorities: accessToken.authorities, 
+			accessToken: oauthResp.access_token, 
+			expires: accessToken.exp, 
+			refreshToken: oauthResp.refresh_token
+		};
+	}
+	
+	getToken(): string {
+		try {
+			let user = sessionStorage.getItem('currentUser');
+			if(user) {
+				return JSON.parse(user).accessToken;
+			}
+		} catch(error) {
+		}
+		
+ 		return null;
+	}
+	
+	authorise(allowedRoles: string[]): boolean {
+		
+		if(this.currentUserSubject.value && this.currentUserSubject.value.authorities) {
+			if(! allowedRoles) {
+				return true;
+			} 
+		
+			// intersection - at least one role match				
+			return allowedRoles.filter(value => -1 !== this.currentUserSubject.value.authorities.indexOf(value)).length > 0;
+		}
+
+		return false;
+	}
 }
